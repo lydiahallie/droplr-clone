8189d91efd39540caf828d7f8ef24a80c0290cd0
Lydia Hallie <lydiajuliettehallie@gmail.com>
2018-06-19 09:54:56 +0200

    Add database

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { storage } from './firebase';
+import { storage, database } from './firebase';
 import FileInput from 'react-file-input';
 
 class App extends Component {
   constructor() {
     super();
     this.handleFileSelected = this.handleFileSelected.bind(this);
-    this.storageRef = storage.ref('/user-images').child('test');
+    this.storageRef = storage.ref('/user-images')
+    this.picturesRef = database.ref('/pictures')
+    this.state = {
+      img: ''
+    }
   }
 
   handleFileSelected(event) {
-    console.log('file!', event.target.files[0])
+    const file = event.target.files[0]
+    const uploadTask = this.storageRef.child('/test').put(file, { contentType: file.type });
+    const image = this.storageRef.child('/test').child(file.name)
+    image.getDownloadURL().then(url => {
+      this.picturesRef.child('/photoURL').set(url)
+      this.setState({ img: url})
+    })
   }
 
   render() {
+    const { files } = this.state
     return (
-      <FileInput 
-        accepts=".jpg, .png, .gif"
-        onChange={ this.handleFileSelected }
-      />
+      <div>
+        <FileInput 
+          accepts=".jpg, .png, .gif"
+          onChange={ this.handleFileSelected }
+        />
+        <img src={this.state.img} />
+      </div>
     );
   }
 }
